16c0463481e7579917dd20a40cd85636d88ffa3a
nbyrd9 <nianibyrd@gmail.com>
2021-03-25 18:53:31 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+    has_many :rides
+    has_many :attractions, through: :rides
+    has_secure_password
+  
+    def mood
+        unless admin
+        self.nausea > self.happiness ? 'sad' : 'happy'
+        end
+    end
 end
